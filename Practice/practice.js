f2c225738a3fcd6049236772d0166f81178152bf
bamaswampmonster <bamaswampmonster@bamaswastersmbp.attlocal.net>
2015-10-15 22:44:00 -0500

    practice finished

---
+++ Practice/practice.js
@@ -0,0 +1,11 @@
+function clickFirst(){
+	alert("Your job is to make two colored boxes appear that give alerts when clicked");
+};
+
+function clickSEcond(){
+	alert("I'm red");
+};
+
+function clickThird(){
+	("I'm blue")
+};
