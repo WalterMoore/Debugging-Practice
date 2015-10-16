06f319fe83d61cf9229d5d0a55f4776d1eb759fc
bamaswampmonster <bamaswampmonster@bamaswastersmbp.attlocal.net>
2015-10-15 22:11:09 -0500

    solution finished

---
+++ Solutions/solution.js
@@ -0,0 +1,11 @@
+function clickFirst(){
+	alert("Your job is to make two colored boxes appear that give alerts when clicked");
+};
+
+function clickSecond(){
+	alert("I'm red");
+};
+
+function clickThird(){
+	alert("I'm blue");
+};
